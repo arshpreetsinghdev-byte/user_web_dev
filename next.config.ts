320ee04886b7406f66b5f33769001368e6c6e651
Arshpreet Singh <karanzirakpur@gmail.com>
2026-03-02 15:11:16 +0530

    (feat) updated next config for the user web

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,28 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://js.stripe.com https://web.squarecdn.com https://connect.squareup.com https://chat.hippochat.io https://hscriptscdnnew.ec2dashboard.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://chat.hippochat.io https://hscriptscdnnew.ec2dashboard.com",
+              "font-src 'self' https://fonts.gstatic.com https://chat.hippochat.io https://hscriptscdnnew.ec2dashboard.com",
+              "img-src 'self' data: https: blob:",
+              "connect-src 'self' https://maps.googleapis.com https://api.stripe.com https://connect.squareup.com https://chat.hippochat.io wss://chat.hippochat.io https://hscriptscdnnew.ec2dashboard.com https://*.jugnoo.in",
+              "frame-src https://js.stripe.com https://connect.squareup.com https://chat.hippochat.io https://widget.hippochat.io",
+          ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+ 
